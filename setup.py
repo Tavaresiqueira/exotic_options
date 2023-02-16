7e9b2b142fd38cd44a04825b8abd9c3d12d5bc8c
Tavaresiqueira <siqueiratav@gmail.com.br>
2023-02-16 06:53:06 -0300

    main V3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='exotic_options',
-      version='0.1',
+      version='0.2',
       description='The Formula to calculate some exotic options.',
       long_description=open('README.md').read(),
       long_description_content_type='text/markdown',
